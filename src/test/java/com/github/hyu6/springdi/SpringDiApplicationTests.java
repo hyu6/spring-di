615730bb504a6c318099a4d259a33cc4529fc483
Haoran Yu <haoranyu6@gmail.com>
2020-03-29 07:51:28 -0700

    initialize the project

---
+++ src/test/java/com/github/hyu6/springdi/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hyu6.springdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
